9fd6878a4e9faff31a914be23fa70be68d448463
amitrathee987 <amitrathee987@gitlab.com>
2020-08-09 13:56:53 +0530

    emit createMessage event from client side and this event listen on server side

--- public/js/group/groupchat.js
+++ public/js/group/groupchat.js
@@ -9,5 +9,22 @@ $(document).ready(function(){
      */
     socket.on('connect', function(){
         console.log('yeahh user connected')
+    });
+
+
+    /**
+     * emit (createMessage) event from client side, when user A send message
+     */
+    $('#message-form').on('submit', function(event){
+        event.preventDefault();
+
+        var msg = $('#msg').val();
+
+        /**
+         * In event, first 
+         */
+        socket.emit('createMessage', {
+            text: msg
+        });
     })
 });
--- socket/groupchat.js
+++ socket/groupchat.js
@@ -3,6 +3,14 @@
  */
 module.exports = function(io){
     io.on('connection', (socket) => {
-        console.log('user connected')
+        console.log('user connected');
+
+        /**
+         * listen createMessage event from client side
+         *
+         */
+        socket.on('createMessage',(message) => {
+            console.log(message);
+        })
     })
 }
